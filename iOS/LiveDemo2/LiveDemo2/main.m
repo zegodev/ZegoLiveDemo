7d1c7fde84b5851b321b2ff86f0db7a7d3e378f6
jiangningb <dev@zego.im>
2016-05-23 16:01:18 +0800

    修改目录结构

---
+++ iOS/LiveDemo2/LiveDemo2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LiveDemo2
+//
+//  Created by Randy Qiu on 4/1/16.
+//  Copyright © 2016 Zego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
